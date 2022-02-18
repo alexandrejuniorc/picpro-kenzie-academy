d16dc59b7272c9d3782edd0e9b3d3c460f181953
Alexandre Junior <alexandrejunior408@gmail.com>
2022-02-17 22:18:13 -0300

    adiciona primeiro exercicio de javascript

---
+++ js/exercicio1/exercicio1.js
@@ -0,0 +1,7 @@
+// Declare uma variável chamada 'nome'
+// Atribua um nome como valor da variável
+// Descomente a última linha
+
+let nome = "Alexandre Junior";
+
+alert("Olá, seja bem-vindo(a), " + nome + "!");
