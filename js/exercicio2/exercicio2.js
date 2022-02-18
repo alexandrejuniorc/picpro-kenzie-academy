4a51f87346986c5032fa5ada2158555bd52d6d86
Alexandre Junior <alexandrejunior408@gmail.com>
2022-02-17 22:21:52 -0300

    adiciona segundo exercicio de javascript

---
+++ js/exercicio2/exercicio2.js
@@ -0,0 +1,10 @@
+// Hora de incrementar um pouco!
+// Declare, uma variável chamada 'primeiroNome'
+// E também uma variável chamada 'ultimoNome'
+// Atribua o seu primeiro e último nome às respectivas variáveis
+// Descomente a linha 10
+
+let primeiroNome = "Alexandre";
+let ultimoNome = "Junior";
+
+alert("Meu nome completo é: " + primeiroNome + " " + ultimoNome);
